c69e504ad9f2d00b270258c6d3eaab5437bddc7a
AbdullakhMirfayziev <abdullakhmirfayziev81@gmail.com>
2024-11-23 03:11:13 +0500

    project started

---
+++ src/main/java/atomic/picture/media/com/socialchat/SocialchatApplication.java
@@ -0,0 +1,13 @@
+package atomic.picture.media.com.socialchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atomic/picture/media/com/socialchat/SocialchatApplicationTests.java
@@ -0,0 +1,13 @@
+package atomic.picture.media.com.socialchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
